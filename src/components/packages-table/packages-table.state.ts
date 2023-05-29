6009cb818a1f61b5fe0f7dbf70f022d5750cb92a
Masha <teenderrr@gmail.com>
2023-05-29 13:06:26 +0300

    feature/add-button

--- src/components/packages-table/packages-table.state.ts
+++ src/components/packages-table/packages-table.state.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppSelector } from 'store';
 
 export const usePackagesTable = () => {
@@ -8,6 +8,14 @@ export const usePackagesTable = () => {
   const handleOpenAdd = () => {
     setIsOnAdd(!isOnAdd);
   };
+
+  useEffect(() => {
+    const elementScroll = document.getElementById('scroll');
+
+    if (packages.length === 0 && elementScroll) {
+      elementScroll.scrollLeft += 500;
+    }
+  }, []);
 
   return {
     isOnAdd, packages, packagesOnShow, handleOpenAdd,
